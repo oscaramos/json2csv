613f6b316c69824d6d7cc1e23c00ec658b4bef48
Oscar <oscar.ramos@ucsp.edu.pe>
2020-05-09 02:40:18 -0500

    Better responsive + PWA + deployement

--- src/components/form-container/FormContainer.jsx
+++ src/components/form-container/FormContainer.jsx
@@ -5,23 +5,25 @@ import JsonContainer from "../json-container/JSONContainer";
 import ConvertButton from "../convert-button/ConvertButton";
 import CSVContainer from "../csv-container/CSVContainer";
 
-export const FormContainer = ({ state, onChangeForm, onConvertClick, onClear, onInputFile }) => (
-  <Grid container justify='center' alignItems='center' style={{ minHeight: "100vh" }}>
-    <Grid item xs={8}>
-      <Paper elevation={4} style={{ height: "50vh" }}>
-        <Grid container justify='center' alignItems='center' style={{ height: "50vh" }}>
-          <Grid item xs={5}>
-            <JsonContainer value={state.json} onChangeForm={onChangeForm} onInputFile={onInputFile}/>
+export const FormContainer = ({ state, onChangeForm, onConvertClick, onClear, onInputFile }) => {
+  return (
+    <Grid container justify='center' alignItems='center' style={{ minHeight: "100vh" }}>
+      <Grid item sm={12} md={10} lg={8} xl={6}>
+        <Paper elevation={4} style={{ height: "50vh" }}>
+          <Grid container justify='center' alignItems='center' style={{ height: "50vh" }}>
+            <Grid item xs={5}>
+              <JsonContainer value={state.json} onChangeForm={onChangeForm} onInputFile={onInputFile} />
+            </Grid>
+            <Grid container item xs={2} justify='center' alignItems='center'>
+              <ConvertButton onClick={onConvertClick} />
+            </Grid>
+            <Grid item xs={5}>
+              <CSVContainer value={state.csv} onChangeForm={onChangeForm} onClear={onClear} />
+            </Grid>
           </Grid>
-          <Grid container item xs={1} justify='center' alignItems='center'>
-            <ConvertButton onClick={onConvertClick} />
-          </Grid>
-          <Grid item xs={5}>
-            <CSVContainer value={state.csv} onChangeForm={onChangeForm} onClear={onClear}/>
-          </Grid>
-        </Grid>
-      </Paper>
-    </Grid>
-  </Grid>);
+        </Paper>
+      </Grid>
+    </Grid>);
+};
 
 export default FormContainer;
