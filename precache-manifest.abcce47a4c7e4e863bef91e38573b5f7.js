90099e5ee56f2f889757846f4af72950328ae222
Oscar <oscar.ramos@ucsp.edu.pe>
2020-05-09 02:08:52 -0500

    Updates

---
+++ precache-manifest.abcce47a4c7e4e863bef91e38573b5f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca4cdc5508b9c1c197c2235a34876ec0",
+    "url": "/json2csv/index.html"
+  },
+  {
+    "revision": "4bdb8ef1e45ce4835898",
+    "url": "/json2csv/static/css/main.dc0a14bb.chunk.css"
+  },
+  {
+    "revision": "adc49b684ba8cac54c8c",
+    "url": "/json2csv/static/js/2.964e9969.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/json2csv/static/js/2.964e9969.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bdb8ef1e45ce4835898",
+    "url": "/json2csv/static/js/main.caca5520.chunk.js"
+  },
+  {
+    "revision": "e7f27ff5100cf4dcfdbc",
+    "url": "/json2csv/static/js/runtime-main.369eb432.js"
+  }
+]);
