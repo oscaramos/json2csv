e7e028dad9a3562c5db22c5e1015ee3c798108ef
Oscar <oscar.ramos@ucsp.edu.pe>
2020-08-15 20:45:31 -0500

    Cleaning until scratch

--- src/App.js
+++ src/App.js
@@ -1,50 +1,11 @@
-import React from 'react';
-import FormContainer from "./components/form-container/FormContainer";
-import json2csv from "./components/json2csv/json2csv.component";
-import './App.css';
+import React from 'react'
 
-class App extends React.Component {
-  state = {
-    json: JSON.stringify({ a: "A", b: "B", c: "C" }, null, 4),
-    csv: '',
-    error: ''
-  }
-
-  onJSON2CSV = () => {
-    try {
-      this.setState({ csv: json2csv(this.state.json) })
-    } catch (e) {
-      this.setState({ error: 'Cant convert Json2Csv' });
-    }
-  }
-
-  onChangeForm = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  }
-
-  onClear = () => {
-    this.setState({
-      json: '',
-      csv: ''
-    });
-  }
-
-  onInputFile = (content) => {
-    this.setState({ json: content });
-  }
-
-  render() {
-    if (this.state.error) throw this.state.error;
-    return (
-      <div>
-        <FormContainer state={this.state}
-                       onChangeForm={this.onChangeForm}
-                       onConvertClick={this.onJSON2CSV}
-                       onClear={this.onClear}
-                       onInputFile={this.onInputFile} />
-      </div>
-    );
-  }
+const App = () => {
+	return (
+		<div>
+			Ready to make reboot
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/components/download-file/downloadFile.js
+++
@@ -1,7 +0,0 @@
-import fileDownload from "js-file-download";
-
-const downloadFile = (data, filename) => {
-  fileDownload(data, filename);
-}
-
-export default downloadFile;
