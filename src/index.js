3e718cb946cf7183d1c1c69cf2efffedd8b1adcc
Oscar <oscar.ramos@ucsp.edu.pe>
2020-08-16 02:44:28 -0500

    Theming

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import * as serviceWorker from './serviceWorker'
 
-import App from './App';
+import { ThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import theme from './theme'
 
-import './index.css';
+import App from './App'
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/theme.js
@@ -0,0 +1,19 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+
+const MuiManatee = '#8D99AE'
+const MuiImperialRed = '#EF233C'
+const MuiAliceBlue = '#edf2f4'
+
+export default createMuiTheme({
+  palette: {
+  	primary: {
+  		main: MuiManatee
+		},
+		secondary: {
+  		main: MuiImperialRed
+		},
+		background: {
+  		default: MuiAliceBlue
+		}
+	}
+});
